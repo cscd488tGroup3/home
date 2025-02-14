2c90a90b755acef88d256aab27086251d3816e33
Ethan Crawford <ecrawford4@ewu.edu>
2025-02-14 04:15:56 +0000

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// https://astro.build/config
 export default defineConfig({
   // your existing configuration
 });
