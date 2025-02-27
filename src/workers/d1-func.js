203f31c8f06ba8b8e63215e02c0a5863e845a9dc
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-02-26 17:58:45 -0800

    add write functions

--- src/workers/d1-func.js
+++ src/workers/d1-func.js
@@ -15,3 +15,30 @@ export async function getInfoByUid(uid, env) {
         throw new Error(`Database query failed: ${err.message}`);
     }
 }
+
+export async function getPassword(uid, env) {
+    try {
+        const { results } = await env.DB.prepare("SELECT hashpass FROM admin WHERE uid = ?").bind(uid).all();
+        return results;
+    } catch (err) {
+        throw new Error(`Database query failed: ${err.message}`);
+    }
+}
+
+export async function writeNewUser(uid, email, fname, lname, dob, doj, env) {
+    try {
+        const {results} = await env.DB.prepare("INSERT INTO info (uid, email, fname, lname, dob, doj) VALUES (?, '?', '?', '?', '?', '?');").bind(uid, email, fname, lname, dob, doj).all();
+        return results;
+    } catch (err) {
+        throw new Error(`Database query failed: ${err.message}`);
+    }
+}
+
+export async function writeNewPassword(uid, hashpass, env) {
+    try {
+        const {results} = await env.DB.prepare("INSERT INTO admin (uid, email, hashpass) VALUES (?, '?', '?');").bind(uid, email, hashpass).all();
+        return results;
+    } catch (err) {
+        throw new Error(`Database query failed: ${err.message}`);
+    }
+}
