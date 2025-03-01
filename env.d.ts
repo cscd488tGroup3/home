d1d9ba4b4cdf184fc131491371f58eba7a8ad5ba
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-02-28 22:24:44 -0800

    env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -2,8 +2,25 @@
 
 interface ImportMetaEnv {
   readonly API_KEY: string;
+  readonly USR_DB: string;
+  readonly USR_DB_W: string;
+  readonly USR_DB_W_ADMIN: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+interface DBConfig {
+  DB: string;
+  DBW: string;
+  DBWA: string;
+}
+
+declare global {
+  interface Window {
+    dbConfig: DBConfig;
+  }
+}
+
+export {};
