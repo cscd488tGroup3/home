2bea7d7f8d0d81fc04a744b2c4f573a23b2f82e9
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-03-07 15:04:25 -0800

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 dotenv.config();
 
