b945a25fdf229c400166927d66af5d5eea30c84b
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-03-07 21:45:20 -0800

    test

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vitest/config';
-import astro from '@astrojs/vite-plugin-astro';
 
 export default defineConfig({
-  plugins: [astro()],
   test: {
     environment: 'happy-dom',
     include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'], // ensure this pattern is correct for your test files
