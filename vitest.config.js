00723c576750c7bb2ddf406069a88202a2e862c3
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-03-07 21:50:38 -0800

    astro config

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
+import { getViteConfig } from 'astro/config';
+export default getViteConfig({
   test: {
     environment: 'happy-dom',
     include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'], // ensure this pattern is correct for your test files
