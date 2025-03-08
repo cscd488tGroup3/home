a7f6e7e330178551979c829f1055301164f5e025
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-03-07 20:48:13 -0800

    clean repo

--- .wrangler/tmp/bundle-f1jGT9/middleware-insertion-facade.js
+++
@@ -1,4 +0,0 @@
-				import { __facade_registerInternal__ } from "C:\\Users\\ethan\\OneDrive\\Documents\\School\\2025\\Winter2025\\CSCD488T\\home\\node_modules\\wrangler\\templates\\middleware\\loader-sw.ts";
-				import * as __MIDDLEWARE_0__ from "C:\\Users\\ethan\\OneDrive\\Documents\\School\\2025\\Winter2025\\CSCD488T\\home\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
-import * as __MIDDLEWARE_1__ from "C:\\Users\\ethan\\OneDrive\\Documents\\School\\2025\\Winter2025\\CSCD488T\\home\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
-				__facade_registerInternal__([__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default])
