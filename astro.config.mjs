7f53b98c7c57b4ba9e25e0320eaf904a3d65e8a9
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-04-04 20:52:28 -0700

    add tailwind to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
 import netlify from '@astrojs/netlify';
+import tailwindcss from '@tailwindcss/vite';
 
 dotenv.config();
 
 export default defineConfig({
   vite: {
+    plugins: [tailwindcss()],
     define: {
       'import.meta.env.API_KEY': JSON.stringify(process.env.API_KEY),
     },
