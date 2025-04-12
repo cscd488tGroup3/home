3965326eb61b2b59dca8d5b4ace5a983df8dc116
Jared Diaz <145868628+JaredD221@users.noreply.github.com>
2025-04-12 16:37:15 +0000

    moved CORS code into new file, also made simple name chages to a few variables in two files. they are "handlePlantRequests" and "queryAPIs"

---
+++ src/pages/api/corsPolicy.js
@@ -0,0 +1,21 @@
+export const getCorsHeaders = (origin) => {
+    const allowedOrigins = [
+        "https://astro-d1-integration.ecrawford4.workers.dev",
+        //"http://localhost:4321", // For local development
+        "https://*--peppy-nougat-0120f1.netlify.app" // For Netlify deployment
+    ];
+
+    return {
+        "Access-Control-Allow-Origin": allowedOrigins.includes(origin) ? origin : "https://peppy-nougat-0120f1.netlify.app", // Default to the first allowed origin
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type"
+    };
+};
+
+export const handleOptionsRequest = (headers) => {
+    return {
+        statusCode: 204,
+        headers,
+        body: "",
+    };
+};
