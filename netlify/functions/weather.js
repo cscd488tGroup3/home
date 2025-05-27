0d5f43c8f690567c075536bb78e499add6c6dd03
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-05-26 19:57:03 -0700

    fix the weather query

---
+++ netlify/functions/weather.js
@@ -0,0 +1,15 @@
+export async function handler(event) {
+    const API_KEY = process.env.API_KEY;
+
+    const body = JSON.parse(event.body);
+    const cityName = body.cityName;
+    const units = body.units;
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${units}&appid=${API_KEY}`);
+
+    if (response.ok) {
+        return response;
+    } else {
+        throw new Error("Query failed.");
+    }
+}
