86111c9f75e93d58894ab4a6c36fbb431a618211
Ethan Crawford <ethan.crawford5532@gmail.com>
2025-05-30 20:16:16 -0700

    add a NewPost component to handle the Cloudinary upload and a script in the Netlify Functions to upload the new post to the database

---
+++ netlify/functions/createPost.js
@@ -0,0 +1,18 @@
+export async function handler(event) {
+    const { pid, uid, caption, cloudinaryUrl } = Object.fromEntries(new URLSearchParams(event.body));
+
+    try {
+        const res = await fetch(`https://astro-d1-integration.ecrawford4.workers.dev/post/create?pid=${pid}&caption=${encodeURIComponent(caption)}&url=${encodeURIComponent(cloudinaryUrl)}&uid=${uid}`);
+        const data = await res.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: err.message }),
+        };
+    }
+}
